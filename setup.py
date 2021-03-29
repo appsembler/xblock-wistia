78cab91a0e9b215fc414b6bf290aa13495da9c64
Omar Al-Ithawi <i@omardo.com>
2021-03-29 10:23:18 +0300

    bump up version to 0.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='wistiavideo-xblock',
-    version='0.3',
+    version='0.4',
     description='wistiavideo XBlock',   # TODO: write a better description.
     license='GPL v3',
     packages=[
